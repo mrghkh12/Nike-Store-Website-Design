ca0090b595957ca686b133fea9b0017bac85d4a8
mohammad reza <mrghkh12@gmail.com>
2024-08-04 23:17:24 +0330

    feat: add feature for active line in navbar menu item

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,17 @@ const $ = document
 const sliderWrapper = $.querySelector('.sliderWrapper')
 const menuItemList = $.querySelectorAll('.menuItem')
 
+const menuItemActiveLine = $.querySelector('.line-active')
+
 menuItemList.forEach((item,index) => {
-    item.addEventListener('click' , () => {
+    
+    item.addEventListener('click' , (e) => {
+        menuItemList.forEach(itemMenu => itemMenu.classList.remove('active'))
         sliderWrapper.style.transform = `translateX(${-100 * index}vw)`
+        menuItemActiveLine.style.left = `${e.target.offsetLeft}px`
+        menuItemActiveLine.style.width = `${e.target.offsetWidth}px`
+
+        e.target.classList.add('active')
     })
 })
 // - dynamic header slider 
